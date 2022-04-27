1753e39a9ae56200bc1ed9a6b8241585c70162f4
Christian Meter <cmeter@googlemail.com>
2022-04-27 17:56:33 +0200

    Convert for to stream

--- src/main/java/de/propra/timetracker/Main.java
+++ src/main/java/de/propra/timetracker/Main.java
@@ -6,19 +6,23 @@ import org.apache.commons.csv.CSVParser;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         readCSV();
     }
 
-    private static void readCSV() throws IOException {
+    private static List<Event> readCSV() throws IOException {
         try (Reader in = new FileReader("src/main/resources/tasks.csv")) {
             CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
                     .setHeader(Spalten.class)
                     .setSkipHeaderRecord(true)
                     .build();
-            CSVParser events = csvFormat.parse(in);
+            CSVParser records = csvFormat.parse(in);
+            return records.stream()
+                    .map(Event::new)
+                    .toList();
         }
     }
 }
