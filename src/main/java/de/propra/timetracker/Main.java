53efef5bec845f95376fed9919fbe5466085c159
Christian Meter <cmeter@googlemail.com>
2022-05-04 17:07:49 +0200

    Add appendCSV to save Events

--- src/main/java/de/propra/timetracker/Main.java
+++ src/main/java/de/propra/timetracker/Main.java
@@ -2,19 +2,27 @@ package de.propra.timetracker;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
 
 public class Main {
+    private static final String TASKS_CSV = "src/main/resources/tasks.csv";
+    private static final CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
+            .setHeader(Spalten.class)
+            .setSkipHeaderRecord(true)
+            .build();
+
     public static void main(String[] args) throws IOException {
         readCSV();
     }
 
     private static List<Event> readCSV() throws IOException {
-        try (Reader in = new FileReader("src/main/resources/tasks.csv")) {
+        try (Reader in = new FileReader(TASKS_CSV)) {
             CSVFormat csvFormat = CSVFormat.DEFAULT.builder()
                     .setHeader(Spalten.class)
                     .setSkipHeaderRecord(true)
@@ -25,4 +33,12 @@ public class Main {
                     .toList();
         }
     }
+
+    private static void appendEvent(Event event) throws IOException {
+        FileWriter out = new FileWriter(TASKS_CSV, true);
+        try (CSVPrinter printer = new CSVPrinter(out, csvFormat)) {
+            printer.printRecord(event.asList());
+        }
+    }
+
 }
