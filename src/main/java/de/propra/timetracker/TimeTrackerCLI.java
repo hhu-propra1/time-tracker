021f859dc9c0955fb88d0f65498164ae31852a49
Christian Meter <cmeter@googlemail.com>
2022-05-11 17:04:54 +0200

    Print help in CLI with -h option

--- src/main/java/de/propra/timetracker/TimeTrackerCLI.java
+++ src/main/java/de/propra/timetracker/TimeTrackerCLI.java
@@ -1,17 +1,44 @@
 package de.propra.timetracker;
 
+import org.apache.commons.cli.*;
+
 enum CLIStatus {
     HELP, ERROR
 }
 
 public class TimeTrackerCLI {
 
+    private static Options options = new Options();
+
     public static void main(String[] args) {
         TimeTrackerCLI timeTrackerCLI = new TimeTrackerCLI();
         timeTrackerCLI.readCLI(args);
     }
 
+    private void hilfe() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("time-tracker", options);
+    }
+
     CLIStatus readCLI(String[] args) {
+        options.addOption("h", "help", false, "Zeige diese Hilfe an");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+            if (cmd.hasOption("h")) {
+                hilfe();
+                return CLIStatus.HELP;
+            }
+
+        } catch (ParseException e) {
+            hilfe();
+            return CLIStatus.ERROR;
+        }
+
+        hilfe();
         return CLIStatus.HELP;
     }
 }
