0051eabb11c6472897bc25b8510aa91d55cb24f8
xor <37473184+xor50@users.noreply.github.com>
2022-05-20 22:25:06 +0200

    Add print table function (#12)
    * add markdown generator dependency to gradle
    
    * add table option to CLI
    
    * add print table functionality

---
+++ src/main/java/de/propra/timetracker/TablePrinter.java
@@ -0,0 +1,16 @@
+package de.propra.timetracker;
+
+import net.steppschuh.markdowngenerator.table.Table;
+import java.util.List;
+
+public class TablePrinter {
+
+	static void printTable(List<Event> events) {
+		Table.Builder tableBuilder = new Table.Builder()
+				.withAlignments(Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT)
+				.addRow("Datum", "Minuten", "Projekt", "Beschreibung");
+
+		events.forEach(e -> tableBuilder.addRow(e.datum(),e.minuten(),e.projekt(),e.beschreibung()));
+		System.out.println(tableBuilder.build());
+	}
+}
