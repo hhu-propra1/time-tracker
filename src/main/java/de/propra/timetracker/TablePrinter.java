ba5aafb148e73a7a57762d464cf29f7d1fdeac5f
dragonempire96 <82451801+dragonempire96@users.noreply.github.com>
2022-05-22 13:39:11 +0200

    deleted command

--- src/main/java/de/propra/timetracker/TablePrinter.java
+++ src/main/java/de/propra/timetracker/TablePrinter.java
@@ -19,7 +19,6 @@ public class TablePrinter {
                 .withAlignments(Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT)
                 .addRow("Datum", "Minuten", "Projekt", "Beschreibung");
 
-        //events.forEach(e -> tableBuilder.addRow(e.datum(),e.minuten(),e.projekt(),e.beschreibung()));
         for (Event event : events) {
             if(event.projekt().equals(projekt)){
                 tableBuilder.addRow(event.datum(),event.minuten(), event.projekt(),event.beschreibung());
