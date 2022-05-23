846fecd87f45bd479b43133479485d5d70de2f3e
dragonempire96 <82451801+dragonempire96@users.noreply.github.com>
2022-05-23 18:17:18 +0200

    reduce redundant code

--- src/main/java/de/propra/timetracker/TablePrinter.java
+++ src/main/java/de/propra/timetracker/TablePrinter.java
@@ -6,9 +6,7 @@ import java.util.List;
 public class TablePrinter {
 
     static void printTable(List<Event> events) {
-        Table.Builder tableBuilder = new Table.Builder()
-                .withAlignments(Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT)
-                .addRow("Datum", "Minuten", "Projekt", "Beschreibung");
+        Table.Builder tableBuilder = getBuilder();
 
         events.forEach(e -> tableBuilder.addRow(e.datum(),e.minuten(),e.projekt(),e.beschreibung()));
         System.out.println(tableBuilder.build());
@@ -16,9 +14,7 @@ public class TablePrinter {
 
     static void printTableOf(List<Event> events, String argument) {
         String[] date = argument.split("");
-        Table.Builder tableBuilder = new Table.Builder()
-                .withAlignments(Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT)
-                .addRow("Datum", "Minuten", "Projekt", "Beschreibung");
+        Table.Builder tableBuilder = getBuilder();
         if(date[4].equals("-") && date[7].equals("-") && date.length==10){
             events.stream().filter(e -> e.datum().equals(argument)).forEach(e -> tableBuilder.addRow(e.datum(),e.minuten(),e.projekt(),e.beschreibung()));
             System.out.println(tableBuilder.build());
@@ -27,6 +23,12 @@ public class TablePrinter {
             events.stream().filter(e -> e.projekt().equals(argument)).forEach(e -> tableBuilder.addRow(e.datum(),e.minuten(),e.projekt(),e.beschreibung()));
             System.out.println(tableBuilder.build());
         }
+    }
+
+    private static Table.Builder getBuilder() {
+        return new Table.Builder()
+                .withAlignments(Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT)
+                .addRow("Datum", "Minuten", "Projekt", "Beschreibung");
     }
 }
 
