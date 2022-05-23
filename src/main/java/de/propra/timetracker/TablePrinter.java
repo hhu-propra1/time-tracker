3d0b8e19d27c1a7c189a8f3b8d6e57aabe7042a1
xor50 <37473184+xor50@users.noreply.github.com>
2022-05-23 18:32:37 +0200

    reduce duplicated code in TablePrinter

--- src/main/java/de/propra/timetracker/TablePrinter.java
+++ src/main/java/de/propra/timetracker/TablePrinter.java
@@ -5,19 +5,21 @@ import java.util.List;
 
 public class TablePrinter {
 
-    static void printTable(List<Event> events) {
-        Table.Builder tableBuilder = new Table.Builder()
+    private static Table.Builder createDefaultTableBuilder(){
+        return new Table.Builder()
                 .withAlignments(Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT)
                 .addRow("Datum", "Minuten", "Projekt", "Beschreibung");
+    }
+
+    static void printTable(List<Event> events) {
+        Table.Builder tableBuilder = createDefaultTableBuilder();
 
         events.forEach(e -> tableBuilder.addRow(e.datum(),e.minuten(),e.projekt(),e.beschreibung()));
         System.out.println(tableBuilder.build());
     }
 
     static void printTableOf(List<Event> events, String projekt) {
-        Table.Builder tableBuilder = new Table.Builder()
-                .withAlignments(Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT, Table.ALIGN_LEFT)
-                .addRow("Datum", "Minuten", "Projekt", "Beschreibung");
+        Table.Builder tableBuilder = createDefaultTableBuilder();
 
         events.stream().filter(e -> e.projekt().equals(projekt)).forEach(e -> tableBuilder.addRow(e.datum(),e.minuten(),e.projekt(),e.beschreibung()));
         System.out.println(tableBuilder.build());
