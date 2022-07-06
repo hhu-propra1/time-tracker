11f95561eaf8962640d54b0c1247d9ed2c8d51a4
Christian Meter <cmeter@googlemail.com>
2022-07-06 15:02:01 +0200

    Fix styling, use try with ressources

--- src/main/java/de/propra/timetracker/TempFile.java
+++ src/main/java/de/propra/timetracker/TempFile.java
@@ -13,21 +13,19 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static org.apache.commons.io.IOUtils.close;
-
 public class TempFile {
     private static final Path DEFAULT_PATH_TO_TEMP = Paths.get(System.getProperty("user.home"), ".config", "time-tracker", "temp.txt");
 
-    static void write(String project){
+    static void write(String project) {
         long minutes = TimeUnit.MILLISECONDS.toMinutes(System.currentTimeMillis());
-        String string = LocalDate.now()+"\n"+ minutes +"\n"+ project;
+        String string = LocalDate.now() + "\n" + minutes + "\n" + project;
         try {
-            if(!Files.exists(DEFAULT_PATH_TO_TEMP)){
+            if (!Files.exists(DEFAULT_PATH_TO_TEMP)) {
                 Files.createDirectories(DEFAULT_PATH_TO_TEMP.getParent());
                 Files.createFile(DEFAULT_PATH_TO_TEMP);
                 File file = new File(String.valueOf(DEFAULT_PATH_TO_TEMP));
                 FileUtils.writeStringToFile(file, string, "UTF-8");
-            } else{
+            } else {
                 System.out.println("Bitte --stop <Beschreibung> erst aufrufen, bevor --start <Projekt>");
             }
         } catch (IOException e) {
@@ -39,10 +37,8 @@ public class TempFile {
     static List<String> read() {
         List<String> lines;
 
-        try {
-            FileReader file = new FileReader(String.valueOf(DEFAULT_PATH_TO_TEMP));
+        try (FileReader file = new FileReader(String.valueOf(DEFAULT_PATH_TO_TEMP))) {
             lines = IOUtils.readLines(file);
-            close(file);
             Files.delete(DEFAULT_PATH_TO_TEMP);
         } catch (IOException e) {
             e.printStackTrace();
